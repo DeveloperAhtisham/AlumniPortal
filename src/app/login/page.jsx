ba63bf6f59b8abaa5aa6bbcc2da1c8995aa1099c
DeveloperAhtisham <ahtishamjutt70fsd@gmail.com>
2025-04-21 06:35:36 +0500

    Credentials

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -19,8 +19,8 @@ function Page() {
 
   const [error, setError] = useState("");
   const [inputs, setInputs] = useState({
-    email: "ahtisham@gmail.com",
-    password: "abc123",
+    email: "mrehman@gmail.com",
+    password: "3456789",
   });
 
   const [isLoading, setLoading] = useState(false);
